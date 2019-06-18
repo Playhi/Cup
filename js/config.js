8d6843f1a7d814c08a65633a5a402e445934f542
Playhi <000902play@gmail.com>
2019-06-18 11:09:21 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@ var __apiKeys = [
 	'm781771530-5367f3401728271078cd7337', //Main
 	'm781771571-941c4c90f91452b9a6e61c78', //Wiki
 	'm781771575-0f8fe29af858bb6b284f7ac0', //FreezeYou
-	'm781771597-4fac452f63e2872771fd67f3', //Forum
 	'm782925225-72029296f1167aef80b98730', //自冻
+	'm781771597-4fac452f63e2872771fd67f3', //Forum
 ];
 
 // refresh interval (in seconds)

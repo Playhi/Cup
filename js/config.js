153800f8b089c7daa17c646a830b47baf1da21c5
Playhi <000902play@gmail.com>
2019-07-01 21:51:12 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,12 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm781771530-5367f3401728271078cd7337', //Main
-	'm781771571-941c4c90f91452b9a6e61c78', //Wiki
-	'm781771575-0f8fe29af858bb6b284f7ac0', //FreezeYou
-	'm782925225-72029296f1167aef80b98730', //自冻
-	'm781771597-4fac452f63e2872771fd67f3', //Forum
+	'm781771530-5367f3401728271078cd7337', //Playhi.Net
+	'm783011813-f95805871cb5419d8a3778ba', //Playhi.Cf
+	'm781771571-941c4c90f91452b9a6e61c78', //Wiki.Playhi.Net
+	'm781771575-0f8fe29af858bb6b284f7ac0', //FreezeYou.Playhi.Net
+	'm782925225-72029296f1167aef80b98730', //Zidon.Net
+	'm781771597-4fac452f63e2872771fd67f3', //Forum.Playhi.Net
+	'm783011826-c7a8d301cc10c507de4c26d1', //App.Playhi.Net
 ];
 
 // refresh interval (in seconds)
